226b49bdeabdbc03ef2d9b279be0d967c575ce84
Gabriel Hilal <gabrielhilal@gmail.com>
2015-07-03 02:34:34 -0300

    add routes to the needed actions in the controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  resources :entries, only: [:new, :create, :destroy]
+  root 'entries#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
